fef3b9493fffc6113b93bf3c7239fbb33051a8c2
Alisson Leiva Salazar <tobiotenma@hotmail.com>
2018-03-21 00:09:16 -0700

    Latched .js

--- catClicker.js
+++ catClicker.js
@@ -1,3 +1,10 @@
-var button;
+function catClicker() {
+    var cat = document.getElementById("container").style.display='none';
+    if (cat.style.display === "block") {
+        cat.style.display = "block";
 
-button++;
+
+    } else {
+        cat.style.display = "none";
+    }
+}
