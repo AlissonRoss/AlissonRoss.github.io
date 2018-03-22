1276daee5c331c1f8f882c27acab4f8a925cf88b
NathanAnthonyRoss19 <NathanAnthonyRoss19@gmail.com>
2018-03-21 18:49:03 -0700

    Replaced single static cat with JS that spawns and recycles cats.

--- catClicker.js
+++ catClicker.js
@@ -1,13 +1,37 @@
+let main = document.getElementById("main");
 let cat = document.getElementById("catGIF");
 let counter = document.getElementById("counter");
 
 let count = 0;
 
+let catPool = [];
+
 function catClick() {
+    /*
     cat.style.animation = 'none';
-    cat.offsetHeight; /* trigger reflow */
+    cat.offsetHeight;
     cat.style.animation = null;
+    */
+    
+    let newCat;
+    if (catPool.length !== 0) {
+      newCat = catPool.pop();
+    } else {
+      newCat = document.createElement("img");
+      newCat.src = "cat.gif";
+      newCat.classList.add("catGIF");
+    }
+    
+    newCat.style.left = (Math.random() * window.innerWidth) + "px";
+    main.append(newCat);
+    
+    setTimeout(recycleCat, 1200, newCat);
     
     ++count;
     counter.innerHTML = count;
+}
+
+function recycleCat(cat) {
+  main.removeChild(cat);
+  catPool.push(cat);
 }
