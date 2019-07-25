03edc1beb700d38c6f7840d38cdc1af8c6039751
Alisson Leiva Salazar <alisson@alisson.me>
2019-07-24 18:11:33 -0700

    Blinking Stars.
    Thanks to Nate for line 24.

--- star.js
+++ star.js
@@ -1,8 +1,10 @@
 function populateStar() {
     let star = document.getElementsByClassName("star")[0];
     let desktop = document.getElementsByClassName("desktop")[0];
+
     //amount of random of stars
     let random = Math.floor(Math.random() * 100) + 10;
+
     for (let loop = 0; loop < random; loop++) {
 
         //populate within the background absolute of class desktop
@@ -11,8 +13,15 @@ function populateStar() {
         newStar.style.left = (Math.random() * window.innerWidth) + "px";
         newStar.style.top = (Math.random() * window.innerHeight) + "px";
         desktop.append(newStar);
+
+        setInterval(blinkStar, 1000, newStar);
     }
+    setInterval(blinkStar, 1000, star);
 }
 //resize function here
+//blink
+function blinkStar(star) {
+    star.style.opacity = Math.random();
+}
 
 populateStar();
