c65d55bb93a1be66e84d5a0d0eba3be08438fbf6
Alisson Ross <alirosssalazar@gmail.com>
2022-03-09 21:22:38 -0800

    Sass, tsx, creating home page on Typescript React

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React, {FC, Fragment} from "react";
+//import NavBar from "";
+
+const Home: FC = (): JSX.Element => { {
+
+      return (
+         <section className="homePage">
+             <NavBar/>
+        </section>
+      );
+   }
+}
+export default Home;
