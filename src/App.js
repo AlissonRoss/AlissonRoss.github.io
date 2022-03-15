9cb7ef965e8a1c613f1fef9a02906c73cbfa7fe2
Alisson Ross <alirosssalazar@gmail.com>
2022-03-15 09:29:56 -0700

    Added footer and more content while deleting more old content

--- src/App.js
+++ src/App.js
@@ -9,24 +9,32 @@ function App() {
       <header className="App-header">
         
         <img src={logo} className="App-logo" alt="logo" />
-        Alisson's Website is now in ReactJS
+        Alisson Ross
         
       </header>
       <main className='App-main'>
         <Box className="Box-main">
-          <p>Hello World from reactjs!</p>
-          <p>Site Currently being migrated to ReactJS.</p>
-          <a
-            className="App-link"
-            href="https://github.com/AlissonRoss"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Alisson's Github
-          </a>
+          Hello World!<br/>
+          <p>My name is Alisson Ross.<br/>
+            I am a Computer Science and Engineering major at Merced Community College.<br/>
+            I am also a Front-end web developer for HackMerced and a IT Assistant for SJDC.<br/>
+            I make website components using ReactJS and other front-end tools.<br/>
+            This site is under current re-construction. <br/>
+          </p>
+          
         </Box>
           
       </main>
+      <footer className='App-footer'>
+        <a
+          className="App-link"
+          href="https://github.com/AlissonRoss"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <br/>Made with &#128151; at Alisson's Github
+        </a>
+      </footer>
     </div>
     
   );
