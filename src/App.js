9a02c54704b7815ae1c01a4ad4e3b8ef7a8bbd7d
Alisson Ross <alirosssalazar@gmail.com>
2022-03-25 21:04:59 -0700

    Styling updates and deleted more files that are no longer needed

--- src/App.js
+++ src/App.js
@@ -3,27 +3,56 @@ import React from 'react';
 import logo from '../src/assets/star.svg';
 import './App.css';
 import Box from '@material-ui/core/Box';
+import { ImageList } from '@material-ui/core';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import ListSubheader from '@material-ui/core/ListSubheader';
+//import PROJECTS from '../src/assets/projects.ts';
+
 function App() {
   return (
     <div className="App">
+      <Box className="NavBar">
+        <ul>
+          <a href="./">Home</a>
+          <a href="https://github.com/AlissonRoss?tab=repositories">Projects</a>
+          <a href="https://www.linkedin.com/in/alisson-ross/">LinkedIn</a>
+        </ul> 
+      </Box>
       <header className="App-header">
         
         <img src={logo} className="App-logo" alt="logo" />
         Alisson Ross
         
       </header>
+  
       <main className='App-main'>
         <Box className="Box-main">
-          Hello World!<br/>
-          <p>My name is Alisson Ross.<br/>
-            I am a Computer Science and Engineering major at Merced Community College.<br/>
-            I am also a Front-end web developer for HackMerced and a IT Assistant for SJDC.<br/>
+          <div className="Hello-world">Hello World!<br/></div>
+          My name is Alisson Ross.<br/>
+            I am a Computer Science and Engineering major at Merced Community College.
+            I am also a Front-end web developer for HackMerced and a IT Assistant for SJDC.
             I make website components using ReactJS, Typescript, and other front-end tools.<br/>
-            This site is under current re-construction. <br/>
-          </p>
+            This site is under current re-construction.
+        
           
         </Box>
-          
+        {/*Thinking wether to add a Carousel or a ImageList */}
+        {/* <ImageList className="ImageList-projects">
+          <ImageListItem key="Subheader" cols={2}>
+            <ListSubheader component="div">Projects</ListSubheader>
+          </ImageListItem>
+        </ImageList> */}
+
+          {/* {PROJECTS.map((item) => (
+            <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+          } </ImageListItem>
+        </ImageList>    */}
       </main>
       <footer className='App-footer'>
         <a
---
+++ src/assets/projects.ts
@@ -0,0 +1,11 @@
+import CATSCRATCH from 'src/assets/images/catscratch.png';
+
+const projects=[
+    {
+        img: CATSCRATCH,
+        title: 'CatScratch',
+        url: "../catscratch/",
+    }
+    
+]
+export default projects;
