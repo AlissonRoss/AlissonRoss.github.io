166c0656353c24a61057d78831c7a5ef3834b65a
AlissonRoss <alirosssalazar@gmail.com>
2023-03-03 19:33:40 -0800

    i am now bob ross

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
       <header className="App-header">
         
         <img src={logo} className="App-logo" alt="logo" />
-        Alisson Ross
+        Bob Ross
       </header>
   
       <div className="Body">
