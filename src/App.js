76c55fac452904fe29ac915f72f425bd26c578f2
AlissonRoss <alisson@alisson.me>
2024-01-19 18:30:47 -0800

    Trying BrowserRouter and History

--- src/App.js
+++ src/App.js
@@ -3,30 +3,34 @@ import Landing from './components/Landing/index.js';
 import MainDesc from './components/LandingDesc/index.js';
 import ProjDesc from './components/ProjDesc/index.js';
 import CertDesc from './components/Certifications/index.js';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
+import { createHashHistory } from 'history';
 
 
 function App() {
   return (
-    <HashRouter>
+    <Router history={createHashHistory({
+      basename: "",
+      hashType: "slash",
+    })}>
       <Routes basename={process.env.PUBLIC_URL}>
-        <Route path="/" element={<Landing />}>
+        <Route element={<Landing />}>
           <Route
             path="/"
             element={<MainDesc />}
           />
           <Route
-            path="proj"
+            path="/proj"
             element={<ProjDesc />}
           />
           <Route
-            path="certs"
+            path="/certs"
             element={<CertDesc />}
           />
         </Route>
       </Routes>
-    </HashRouter>
+    </Router>
    
     
   );
